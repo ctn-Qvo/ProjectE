728f5e3366b31e05b7a61e928f0651830d887d54
梦依依OVO <3647844952@qq.com>
2026-08-19 17:21:19 +0800

    修复崩溃bug增加空指针检查

--- src/main/java/moze_intel/projecte/gameObjs/registries/PECreativeTabs.java
+++ src/main/java/moze_intel/projecte/gameObjs/registries/PECreativeTabs.java
@@ -19,139 +19,142 @@ public class PECreativeTabs {
 
 	public static final CreativeTabRegistryObject PROJECTE = CREATIVE_TABS.registerMain(PELang.PROJECTE, PEItems.PHILOSOPHERS_STONE, builder ->
 			builder.displayItems((displayParameters, output) -> {
-				output.accept(PEItems.PHILOSOPHERS_STONE);
-				output.accept(PEItems.REPAIR_TALISMAN);
-				output.accept(PEItems.TOME_OF_KNOWLEDGE);
-				output.accept(PEItems.TRANSMUTATION_TABLET);
-				output.accept(PEBlocks.TRANSMUTATION_TABLE);
+				safeAccept(output, PEItems.PHILOSOPHERS_STONE);
+				safeAccept(output, PEItems.REPAIR_TALISMAN);
+				safeAccept(output, PEItems.TOME_OF_KNOWLEDGE);
+				safeAccept(output, PEItems.TRANSMUTATION_TABLET);
+				safeAccept(output, PEBlocks.TRANSMUTATION_TABLE);
 
-				output.accept(PEItems.LOW_COVALENCE_DUST);
-				output.accept(PEItems.MEDIUM_COVALENCE_DUST);
-				output.accept(PEItems.HIGH_COVALENCE_DUST);
+				safeAccept(output, PEItems.LOW_COVALENCE_DUST);
+				safeAccept(output, PEItems.MEDIUM_COVALENCE_DUST);
+				safeAccept(output, PEItems.HIGH_COVALENCE_DUST);
 
-				output.accept(PEItems.ALCHEMICAL_COAL);
-				output.accept(PEItems.MOBIUS_FUEL);
-				output.accept(PEItems.AETERNALIS_FUEL);
-				output.accept(PEItems.DARK_MATTER);
-				output.accept(PEItems.RED_MATTER);
-				output.accept(PEBlocks.ALCHEMICAL_COAL);
-				output.accept(PEBlocks.MOBIUS_FUEL);
-				output.accept(PEBlocks.AETERNALIS_FUEL);
-				output.accept(PEBlocks.DARK_MATTER);
-				output.accept(PEBlocks.RED_MATTER);
+				safeAccept(output, PEItems.ALCHEMICAL_COAL);
+				safeAccept(output, PEItems.MOBIUS_FUEL);
+				safeAccept(output, PEItems.AETERNALIS_FUEL);
+				safeAccept(output, PEItems.DARK_MATTER);
+				safeAccept(output, PEItems.RED_MATTER);
+				safeAccept(output, PEBlocks.ALCHEMICAL_COAL);
+				safeAccept(output, PEBlocks.MOBIUS_FUEL);
+				safeAccept(output, PEBlocks.AETERNALIS_FUEL);
+				safeAccept(output, PEBlocks.DARK_MATTER);
+				safeAccept(output, PEBlocks.RED_MATTER);
 
-				output.accept(PEItems.KLEIN_STAR_EIN);
-				output.accept(PEItems.KLEIN_STAR_ZWEI);
-				output.accept(PEItems.KLEIN_STAR_DREI);
-				output.accept(PEItems.KLEIN_STAR_VIER);
-				output.accept(PEItems.KLEIN_STAR_SPHERE);
-				output.accept(PEItems.KLEIN_STAR_OMEGA);
+				safeAccept(output, PEItems.KLEIN_STAR_EIN);
+				safeAccept(output, PEItems.KLEIN_STAR_ZWEI);
+				safeAccept(output, PEItems.KLEIN_STAR_DREI);
+				safeAccept(output, PEItems.KLEIN_STAR_VIER);
+				safeAccept(output, PEItems.KLEIN_STAR_SPHERE);
+				safeAccept(output, PEItems.KLEIN_STAR_OMEGA);
 
-				output.accept(PEItems.DARK_MATTER_PICKAXE);
-				output.accept(PEItems.DARK_MATTER_AXE);
-				output.accept(PEItems.DARK_MATTER_SHOVEL);
-				output.accept(PEItems.DARK_MATTER_HOE);
-				output.accept(PEItems.DARK_MATTER_SHEARS);
-				output.accept(PEItems.DARK_MATTER_HAMMER);
-				output.accept(PEItems.DARK_MATTER_SWORD);
+				safeAccept(output, PEItems.DARK_MATTER_PICKAXE);
+				safeAccept(output, PEItems.DARK_MATTER_AXE);
+				safeAccept(output, PEItems.DARK_MATTER_SHOVEL);
+				safeAccept(output, PEItems.DARK_MATTER_HOE);
+				safeAccept(output, PEItems.DARK_MATTER_SHEARS);
+				safeAccept(output, PEItems.DARK_MATTER_HAMMER);
+				safeAccept(output, PEItems.DARK_MATTER_SWORD);
 
-				output.accept(PEItems.RED_MATTER_PICKAXE);
-				output.accept(PEItems.RED_MATTER_AXE);
-				output.accept(PEItems.RED_MATTER_SHOVEL);
-				output.accept(PEItems.RED_MATTER_HOE);
-				output.accept(PEItems.RED_MATTER_SHEARS);
-				output.accept(PEItems.RED_MATTER_HAMMER);
-				output.accept(PEItems.RED_MATTER_MORNING_STAR);
-				output.accept(PEItems.RED_MATTER_SWORD);
-				output.accept(PEItems.RED_MATTER_KATAR);
+				safeAccept(output, PEItems.RED_MATTER_PICKAXE);
+				safeAccept(output, PEItems.RED_MATTER_AXE);
+				safeAccept(output, PEItems.RED_MATTER_SHOVEL);
+				safeAccept(output, PEItems.RED_MATTER_HOE);
+				safeAccept(output, PEItems.RED_MATTER_SHEARS);
+				safeAccept(output, PEItems.RED_MATTER_HAMMER);
+				safeAccept(output, PEItems.RED_MATTER_MORNING_STAR);
+				safeAccept(output, PEItems.RED_MATTER_SWORD);
+				safeAccept(output, PEItems.RED_MATTER_KATAR);
 
 				addArmor(output::accept);
 
-				output.accept(PEItems.DESTRUCTION_CATALYST);
-				output.accept(PEItems.HYPERKINETIC_LENS);
-				output.accept(PEItems.CATALYTIC_LENS);
+				safeAccept(output, PEItems.DESTRUCTION_CATALYST);
+				safeAccept(output, PEItems.HYPERKINETIC_LENS);
+				safeAccept(output, PEItems.CATALYTIC_LENS);
 
-				output.accept(PEItems.IRON_BAND);
-				output.accept(PEItems.BLACK_HOLE_BAND);
-				output.accept(PEItems.ARCHANGEL_SMITE);
-				output.accept(PEItems.HARVEST_GODDESS_BAND);
-				output.accept(PEItems.IGNITION_RING);
-				output.accept(PEItems.ZERO_RING);
-				output.accept(PEItems.SWIFTWOLF_RENDING_GALE);
-				output.accept(PEItems.WATCH_OF_FLOWING_TIME);
-				output.accept(PEItems.EVERTIDE_AMULET);
-				output.accept(PEItems.VOLCANITE_AMULET);
-				output.accept(PEItems.GEM_OF_ETERNAL_DENSITY);
-				output.accept(PEItems.MERCURIAL_EYE);
-				output.accept(PEItems.VOID_RING);
+				safeAccept(output, PEItems.IRON_BAND);
+				safeAccept(output, PEItems.BLACK_HOLE_BAND);
+				safeAccept(output, PEItems.ARCHANGEL_SMITE);
+				safeAccept(output, PEItems.HARVEST_GODDESS_BAND);
+				safeAccept(output, PEItems.IGNITION_RING);
+				safeAccept(output, PEItems.ZERO_RING);
+				safeAccept(output, PEItems.SWIFTWOLF_RENDING_GALE);
+				safeAccept(output, PEItems.WATCH_OF_FLOWING_TIME);
+				safeAccept(output, PEItems.EVERTIDE_AMULET);
+				safeAccept(output, PEItems.VOLCANITE_AMULET);
+				safeAccept(output, PEItems.GEM_OF_ETERNAL_DENSITY);
+				safeAccept(output, PEItems.MERCURIAL_EYE);
+				safeAccept(output, PEItems.VOID_RING);
 
-				for (byte i = 0; i < PEItems.ARCANA_RING.asItem().getModeCount(); ++i) {
-					ItemStack stack = new ItemStack(PEItems.ARCANA_RING);
-					stack.getOrCreateTag().putByte(Constants.NBT_KEY_MODE, i);
-					output.accept(stack);
+				if (PEItems.ARCANA_RING != null) {
+					for (byte i = 0; i < PEItems.ARCANA_RING.asItem().getModeCount(); ++i) {
+						ItemStack stack = new ItemStack(PEItems.ARCANA_RING);
+						stack.getOrCreateTag().putByte(Constants.NBT_KEY_MODE, i);
+						output.accept(stack);
+					}
 				}
 
-				output.accept(PEItems.BODY_STONE);
-				output.accept(PEItems.SOUL_STONE);
-				output.accept(PEItems.MIND_STONE);
-				output.accept(PEItems.LIFE_STONE);
+				safeAccept(output, PEItems.BODY_STONE);
+				safeAccept(output, PEItems.SOUL_STONE);
+				safeAccept(output, PEItems.MIND_STONE);
+				safeAccept(output, PEItems.LIFE_STONE);
 
-				output.accept(PEItems.LOW_DIVINING_ROD);
-				output.accept(PEItems.MEDIUM_DIVINING_ROD);
-				output.accept(PEItems.HIGH_DIVINING_ROD);
+				safeAccept(output, PEItems.LOW_DIVINING_ROD);
+				safeAccept(output, PEItems.MEDIUM_DIVINING_ROD);
+				safeAccept(output, PEItems.HIGH_DIVINING_ROD);
 
-				output.accept(PEItems.WHITE_ALCHEMICAL_BAG);
-				output.accept(PEItems.ORANGE_ALCHEMICAL_BAG);
-				output.accept(PEItems.MAGENTA_ALCHEMICAL_BAG);
-				output.accept(PEItems.LIGHT_BLUE_ALCHEMICAL_BAG);
-				output.accept(PEItems.YELLOW_ALCHEMICAL_BAG);
-				output.accept(PEItems.LIME_ALCHEMICAL_BAG);
-				output.accept(PEItems.PINK_ALCHEMICAL_BAG);
-				output.accept(PEItems.GRAY_ALCHEMICAL_BAG);
-				output.accept(PEItems.LIGHT_GRAY_ALCHEMICAL_BAG);
-				output.accept(PEItems.CYAN_ALCHEMICAL_BAG);
-				output.accept(PEItems.PURPLE_ALCHEMICAL_BAG);
-				output.accept(PEItems.BLUE_ALCHEMICAL_BAG);
-				output.accept(PEItems.BROWN_ALCHEMICAL_BAG);
-				output.accept(PEItems.GREEN_ALCHEMICAL_BAG);
-				output.accept(PEItems.RED_ALCHEMICAL_BAG);
-				output.accept(PEItems.BLACK_ALCHEMICAL_BAG);
+				safeAccept(output, PEItems.WHITE_ALCHEMICAL_BAG);
+				safeAccept(output, PEItems.ORANGE_ALCHEMICAL_BAG);
+				safeAccept(output, PEItems.MAGENTA_ALCHEMICAL_BAG);
+				safeAccept(output, PEItems.LIGHT_BLUE_ALCHEMICAL_BAG);
+				safeAccept(output, PEItems.YELLOW_ALCHEMICAL_BAG);
+				safeAccept(output, PEItems.LIME_ALCHEMICAL_BAG);
+				safeAccept(output, PEItems.PINK_ALCHEMICAL_BAG);
+				safeAccept(output, PEItems.GRAY_ALCHEMICAL_BAG);
+				safeAccept(output, PEItems.LIGHT_GRAY_ALCHEMICAL_BAG);
+				safeAccept(output, PEItems.CYAN_ALCHEMICAL_BAG);
+				safeAccept(output, PEItems.PURPLE_ALCHEMICAL_BAG);
+				safeAccept(output, PEItems.BLUE_ALCHEMICAL_BAG);
+				safeAccept(output, PEItems.BROWN_ALCHEMICAL_BAG);
+				safeAccept(output, PEItems.GREEN_ALCHEMICAL_BAG);
+				safeAccept(output, PEItems.RED_ALCHEMICAL_BAG);
+				safeAccept(output, PEItems.BLACK_ALCHEMICAL_BAG);
 
-				output.accept(PEBlocks.ALCHEMICAL_CHEST);
-				output.accept(PEBlocks.CONDENSER);
-				output.accept(PEBlocks.CONDENSER_MK2);
+				safeAccept(output, PEBlocks.ALCHEMICAL_CHEST);
+				safeAccept(output, PEBlocks.CONDENSER);
+				safeAccept(output, PEBlocks.CONDENSER_MK2);
 
-				output.accept(PEBlocks.COLLECTOR);
-				output.accept(PEBlocks.COLLECTOR_MK2);
-				output.accept(PEBlocks.COLLECTOR_MK3);
-				output.accept(PEBlocks.RELAY);
-				output.accept(PEBlocks.RELAY_MK2);
-				output.accept(PEBlocks.RELAY_MK3);
+				safeAccept(output, PEBlocks.COLLECTOR);
+				safeAccept(output, PEBlocks.COLLECTOR_MK2);
+				safeAccept(output, PEBlocks.COLLECTOR_MK3);
+				safeAccept(output, PEBlocks.RELAY);
+				safeAccept(output, PEBlocks.RELAY_MK2);
+				safeAccept(output, PEBlocks.RELAY_MK3);
 
-				output.accept(PEBlocks.DARK_MATTER_PEDESTAL);
-				output.accept(PEBlocks.DARK_MATTER_FURNACE);
-				output.accept(PEBlocks.RED_MATTER_FURNACE);
-				output.accept(PEBlocks.INTERDICTION_TORCH);
-				output.accept(PEBlocks.NOVA_CATALYST);
-				output.accept(PEBlocks.NOVA_CATACLYSM);
+				safeAccept(output, PEBlocks.DARK_MATTER_PEDESTAL);
+				safeAccept(output, PEBlocks.DARK_MATTER_FURNACE);
+				safeAccept(output, PEBlocks.RED_MATTER_FURNACE);
+				safeAccept(output, PEBlocks.INTERDICTION_TORCH);
+				safeAccept(output, PEBlocks.NOVA_CATALYST);
+				safeAccept(output, PEBlocks.NOVA_CATACLYSM);
 			})
 	);
 
 	private static void addArmor(Consumer<ItemLike> output) {
-		output.accept(PEItems.DARK_MATTER_HELMET);
-		output.accept(PEItems.DARK_MATTER_CHESTPLATE);
-		output.accept(PEItems.DARK_MATTER_LEGGINGS);
-		output.accept(PEItems.DARK_MATTER_BOOTS);
+		Consumer<ItemLike> safe = item -> { if (item != null) output.accept(item); };
+		safe.accept(PEItems.DARK_MATTER_HELMET);
+		safe.accept(PEItems.DARK_MATTER_CHESTPLATE);
+		safe.accept(PEItems.DARK_MATTER_LEGGINGS);
+		safe.accept(PEItems.DARK_MATTER_BOOTS);
 
-		output.accept(PEItems.RED_MATTER_HELMET);
-		output.accept(PEItems.RED_MATTER_CHESTPLATE);
-		output.accept(PEItems.RED_MATTER_LEGGINGS);
-		output.accept(PEItems.RED_MATTER_BOOTS);
+		safe.accept(PEItems.RED_MATTER_HELMET);
+		safe.accept(PEItems.RED_MATTER_CHESTPLATE);
+		safe.accept(PEItems.RED_MATTER_LEGGINGS);
+		safe.accept(PEItems.RED_MATTER_BOOTS);
 
-		output.accept(PEItems.GEM_HELMET);
-		output.accept(PEItems.GEM_CHESTPLATE);
-		output.accept(PEItems.GEM_LEGGINGS);
-		output.accept(PEItems.GEM_BOOTS);
+		safe.accept(PEItems.GEM_HELMET);
+		safe.accept(PEItems.GEM_CHESTPLATE);
+		safe.accept(PEItems.GEM_LEGGINGS);
+		safe.accept(PEItems.GEM_BOOTS);
 	}
 
 	private static void addToExistingTabs(BuildCreativeModeTabContentsEvent event) {
@@ -246,10 +249,12 @@ public class PECreativeTabs {
 					PEItems.VOID_RING
 			);
 
-			for (byte i = 0; i < PEItems.ARCANA_RING.asItem().getModeCount(); ++i) {
-				ItemStack stack = new ItemStack(PEItems.ARCANA_RING);
-				stack.getOrCreateTag().putByte(Constants.NBT_KEY_MODE, i);
-				event.accept(stack);
+			if (PEItems.ARCANA_RING != null) {
+				for (byte i = 0; i < PEItems.ARCANA_RING.asItem().getModeCount(); ++i) {
+					ItemStack stack = new ItemStack(PEItems.ARCANA_RING);
+					stack.getOrCreateTag().putByte(Constants.NBT_KEY_MODE, i);
+					event.accept(stack);
+				}
 			}
 
 			addToExistingTab(event,
@@ -323,9 +328,19 @@ public class PECreativeTabs {
 		}
 	}
 
+	// 安全添加单个 ItemLike 到 CreativeModeTab.Output
+	private static void safeAccept(CreativeModeTab.Output output, ItemLike item) {
+		if (item != null) {
+			output.accept(item);
+		}
+	}
+
+	// 安全添加多个 ItemLike 到 BuildCreativeModeTabContentsEvent
 	private static void addToExistingTab(BuildCreativeModeTabContentsEvent event, ItemLike... items) {
 		for (ItemLike item : items) {
-			event.accept(item);
+			if (item != null) {
+				event.accept(item);
+			}
 		}
 	}
 }
